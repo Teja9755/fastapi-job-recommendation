cdba2ea5b3d586fc4aa94dc45486d199e512a407
Teja Poluru <tejavardhanpoluru@gmail.com>
2025-10-24 00:30:12 +0530

    new changes

--- app/database.py
+++ app/database.py
@@ -16,8 +16,6 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 
 
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
-
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
